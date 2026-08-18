92e7ad48fc455e744791b8d60337532cdeeb30c1
cuytamvan <projectrizkicuy@gmail.com>
2026-08-18 15:16:13 +0700

    feat(ui): change into minimalist design

---
+++ html/js/ui.js
@@ -0,0 +1,16 @@
+(function(){
+  "use strict";
+  var nodes = document.querySelectorAll('.reveal');
+  if (!nodes.length) return;
+  var io = new IntersectionObserver(function(entries){
+    entries.forEach(function(entry){
+      if (!entry.isIntersecting) return;
+      entry.target.classList.add('is-in');
+      io.unobserve(entry.target);
+    });
+  }, { threshold: 0.12, rootMargin: '0px 0px -8% 0px' });
+  nodes.forEach(function(el, i){
+    el.style.setProperty('--index', String(i));
+    io.observe(el);
+  });
+})();
