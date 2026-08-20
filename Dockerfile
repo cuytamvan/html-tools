218e5c4b438c55875c95e57ab34d6976c6524fa4
cuytamvan <projectrizkicuy@gmail.com>
2026-08-20 15:15:57 +0700

    deploy: fix building docker

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ WORKDIR /app
 COPY package.json bun.lock ./
 RUN bun install --frozen-lockfile
 COPY . .
-RUN bun run build
+RUN bunx vite build
 
 FROM nginx:alpine
 COPY nginx.conf /etc/nginx/conf.d/default.conf
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
+
 import App from './App.vue';
 import { Router } from './router';
+
 import { registerPwa } from './pwa';
+
 import './styles/global.css';
 import './styles/landing.css';
 
